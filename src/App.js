54086347af03001110d304195e53dce18bb81d9c
bertino2 <berolbertindjomo@gmail.com>
2024-10-11 15:25:27 +0200

    mise en developpement du site

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ function App() {
   return (
     <div className="App">
       <h1>Bienvenue sur le site de gestion des invités</h1>
+    
+     <h1>Ce site est pour le moment en developpement</h1>
     </div>
   );
 }
